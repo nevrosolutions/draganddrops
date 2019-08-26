55e50019f3a0c98676bf09712f769d825e0e867d
lasa84 <itprofessionals84@gmail.com>
2019-08-26 18:41:28 +0530

    added functions for buttons

--- demodraganddrop/src/app/dashboard/dashboard.component.ts
+++ demodraganddrop/src/app/dashboard/dashboard.component.ts
@@ -7,12 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 private showBasic:boolean=false;
+private showReordering:boolean=false;
+private showAttachingData:boolean=false;
+private showDragAndDropUsingHandlers:boolean=false;
+private showDragAndDropPreview:boolean=false;
+private showDragAndDropCustomizedPlaceHolder:boolean=false;
+
   constructor() { }
 
   ngOnInit() {
   }
 private showBasicFunction(){
-  console.log("Crick info")
+this.flag(false);
 this.showBasic=true;
 }
+private showReorderingFunction(){
+  this.flag(false);
+this.showReordering=true;
+}
+private showAttachingDataFunction(){
+  this.flag(false);
+this.showAttachingData=true;
+}
+private showDragAndDropUsingHandlersFunction(){
+  this.flag(false);
+this.showDragAndDropUsingHandlers=true;
+}
+private showDragAndDropPreviewFunction(){
+  this.flag(false);
+this.showDragAndDropPreview=true;
+}
+private showDragAndDropCustomizedPlaceHolderFunction(){
+  this.flag(false);
+  this.showDragAndDropCustomizedPlaceHolder=true;
+  }
+
+  private flag(flag: boolean) {
+    this.showBasic = flag;
+    this.showReordering = flag;
+    this.showAttachingData = flag;
+    this.showDragAndDropUsingHandlers = flag;
+    this.showDragAndDropPreview = flag;
+    this.showDragAndDropCustomizedPlaceHolder = flag;
+  }
 }
